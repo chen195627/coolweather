9da1a3baa1216a0cf56a8fd84d38184e4299fa10
chen195627 <2984202073@qq.com>
2018-06-20 17:30:52 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/xiaolan/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/xiaolan/coolweather/ChooseAreaFragment.java
@@ -244,8 +244,8 @@ public class ChooseAreaFragment extends Fragment {
                     @Override
                     public void run() {
                         closeProgressDialog();
-                        //Toast.makeText(getContext(), "hello world", Toast.LENGTH_SHORT).show();
-                       // Toast.makeText(getContext(), "加载失败", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "hello world", Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(getContext(), "加载失败", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
